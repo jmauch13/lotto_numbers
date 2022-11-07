7a83eda2235a595e5c9af2424d69f9999872ce17
Jennie Mauch <jenniemauch@me.com>
2022-11-07 11:53:38 -0500

    game functions

--- lucky_lotto.py
+++ lucky_lotto.py
@@ -1,29 +1,12 @@
 import random
 print("Welcome to Lucky Lotto!")
-user_input = ''
-while True:
-    user_input = input(
-        "\n1. National Games \n2. State Games \nPlease select your game type: ")
-    if user_input == "1":
-        print("\nNational Games is a great choice!")
-        national_input = input(
-            "1. Powerball \n2. Mega Millions \nChoose your game: ")
-    elif user_input == "2":
-        print("\nLet's pick a State and get started!")
-        state_choice = input(
-            "1. Ohio \n2. Pennsylvania \n3. New York \nWhich state do you live in? ")
-    else:
-        print("Please choose an option!")
-        break
 
 # National Games
-
-
 def powerball():
     power_nums = random.sample(range(1, 69), 5)
     power_nums.sort()
     bonus_nums = random.sample(range(1, 26), 1)
-    print(power_nums + bonus_nums)
+    print(power_nums + "bonus number",bonus_nums)
 
 
 def mega_millions():
@@ -52,18 +35,16 @@ def classic_lotto():
     classic_nums.sort()
     print(classic_nums)
 
+
 # Pennsylvania & New York
-
-
 def cash4_life():
     cash_nums = random.sample(range(1, 60), 5)
     cash_nums.sort()
     cash_bonus = random.sample(range(1, 4), 1)
     print(cash_nums + cash_bonus)
 
+
 # Pennsylvania Games
-
-
 def match6():
     match6_nums = random.sample(range(1, 49), 6)
     match6_nums.sort()
@@ -75,9 +56,8 @@ def treasure_hunt():
     treasure_nums.sort()
     print(treasure_nums)
 
+
 # New York Games
-
-
 def newyork_game():
     newyork_nums = random.sample(range(1, 59), 6)
     newyork_nums.sort()
@@ -88,3 +68,53 @@ def pick10():
     pick10_nums = random.sample(range(1, 80), 10)
     pick10_nums.sort()
     print(pick10_nums)
+
+
+user_input = ''
+while True:
+    user_input = input(
+        "\n1. National Games \n2. State Games \nPlease select your game type: ")
+    if user_input == "1":
+        print("\nNational Games is a great choice!")
+        national_input = input(
+            "1. Powerball \n2. Mega Millions \nChoose your game: ")
+        if national_input == "1":
+            powerball()
+        elif national_input == "2":
+            mega_millions()
+
+    if user_input == "2":
+        print("\nLet's pick a State and get started!")
+        state_choice = input(
+            "1. Ohio \n2. Pennsylvania \n3. New York \nWhich state do you live in? ")
+        if state_choice == "1":
+            ohio_choices = input(
+                "1. Lucky for Life \n2. Classic Lottery \n3. Rolling Cash 5 \nWhich Ohio game would you like numbers for? ")
+            if ohio_choices == "1":
+                lucky4_life()
+            elif ohio_choices == "2":
+                classic_lotto()
+            elif ohio_choices == "3":
+                rolling_cash()
+        elif state_choice == "2":
+            pennsylvania_choice = input(
+                "1. Cash 4 Life \n2. Match 6 \n3. Treasure Hunt \nWhich Pennsylvania game would you like numbers for? ")
+            if pennsylvania_choice == "1":
+                cash4_life()
+            elif pennsylvania_choice == "2":
+                match6()
+            elif pennsylvania_choice == "3":
+                treasure_hunt()
+        elif state_choice == "3":
+            newyork_choice = input(
+                "1. Cash 4 Life \n2. New York Lottery \n3. Pick 10 \nWhich New York game would you like numbers for? ")
+            if newyork_choice == "1":
+                cash4_life()
+            elif newyork_choice == "2":
+                newyork_game()
+            elif newyork_choice == "3":
+                pick10()
+    else:
+        print("Please choose an option!")
+        break
+
