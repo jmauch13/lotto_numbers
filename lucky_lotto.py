8438ba966e2d49e29ace52a3eea3ac72b9153733
Jennie Mauch <jenniemauch@me.com>
2022-11-10 19:27:44 -0500

    menus created

--- lucky_lotto.py
+++ lucky_lotto.py
@@ -21,7 +21,7 @@ def powerball():
     print("Here are your main Powerball numbers:")
     print(power_nums)
     print("Bonus number:",bonus_nums)
-    print("Good luck!!")
+    print("\nGood luck!!")
 
 
 def mega_millions():
@@ -31,14 +31,14 @@ def mega_millions():
     print("Here are your main Mega Millions numbers:")
     print(mega_nums)
     print("Bonus number:",mega_bonus)
-    print("Good luck!!")
+    print("\nGood luck!!")
 
 # Ohio Games
 def rolling_cash():
     rolling_cash = random.sample(range(1, 49), 6)
     rolling_cash.sort()
     print(rolling_cash)
-    print("Good luck!!")
+    print("\nGood luck!!")
 
 def lucky4_life():
     lucky_nums = random.sample(range(1, 48), 5)
@@ -47,13 +47,13 @@ def lucky4_life():
     print("Here are your main Lucky 4 Life numbers:")
     print(lucky_nums)
     print("Bonus number:",lucky_bonus)
-    print("Good luck!!")
+    print("\nGood luck!!")
 
 def classic_lotto():
     classic_nums = random.sample(range(1, 49), 6)
     classic_nums.sort()
     print(classic_nums)
-    print("Good luck!!")
+    print("\nGood luck!!")
 
 # Pennsylvania & New York
 def cash4_life():
@@ -63,52 +63,93 @@ def cash4_life():
     print("Here are you main Cash 4 Life numbers:")
     print(cash_nums)
     print("Bonus number:",cash_bonus)
-    print("Good luck!!")
+    print("\nGood luck!!")
 
 # Pennsylvania Games
 def match6():
     match6_nums = random.sample(range(1, 49), 6)
     match6_nums.sort()
     print(match6_nums)
-    print("Good luck!!")
+    print("\nGood luck!!")
 
 def treasure_hunt():
     treasure_nums = random.sample(range(1, 30), 5)
     treasure_nums.sort()
     print(treasure_nums)
-    print("Good luck!!")
+    print("\nGood luck!!")
 
 # New York Games
 def newyork_game():
     newyork_nums = random.sample(range(1, 59), 6)
     newyork_nums.sort()
     print(newyork_nums)
-    print("Good luck!!")
+    print("\nGood luck!!")
 
 def pick10():
     pick10_nums = random.sample(range(1, 80), 10)
     pick10_nums.sort()
     print(pick10_nums)
-    print("Good luck!!")
+    print("\nGood luck!!")
 
+def main_menu():
+    print("\n")
+    print("----------------------------------------")
+    print("|              Main Menu:              |")
+    print("----------------------------------------")
+    print("| 1. National Games     2. State Games |")
+    print("----------------------------------------")
+    print("|               3. Exit                |")
+    print("----------------------------------------")
+    print("\n")
+
+def national_menu():
+    print("\n")
+    print("-----------------------------------------")
+    print("|            National Games:            |")
+    print("-----------------------------------------")
+    print("| 1. Powerball         2. Mega Millions |")
+    print("-----------------------------------------")
+    print("|           3. Main Menu                |")
+    print("-----------------------------------------")
+    print("\n")
+
+def state_menu():
+    print("\n")
+    print("-----------------------------------------")
+    print("|             State Games:              |")
+    print("-----------------------------------------")
+    print("| 1. Ohio Games       2. New York Games |")
+    print("-----------------------------------------")
+    print("| 3. Pennsylvania Games   4. Main Menu  |")
+    print("-----------------------------------------")
+    print("\n")
+    
 
 
 while True:
+    main_menu()
     user_input = input(
-        "\n\n1. National Games \n2. State Games \nPlease select your game type: ")
+        "\nPlease select your game type: ")
     if user_input == "1":
         print("\nNational Games is a great choice!")
+        national_menu()
         national_input = input(
-            "\n1. Powerball \n2. Mega Millions \nChoose your game: ")
+            "\nChoose your game: ")
         if national_input == "1":
             powerball()
         elif national_input == "2":
             mega_millions()
+        elif national_input == "3":
+            main_menu()
+    elif user_input == "3":
+        print("Goodbye!")
+        exit()
 
     if user_input == "2":
         print("\nLet's pick a State and get started!")
+        state_menu()
         state_choice = input(
-            "\n1. Ohio \n2. Pennsylvania \n3. New York \nWhich state do you live in? ")
+            "\nWhich state do you live in? ")
         if state_choice == "1":
             ohio_choices = input(
                 "\n1. Lucky for Life \n2. Classic Lottery \n3. Rolling Cash 5 \nWhich Ohio game would you like numbers for? ")
@@ -119,15 +160,6 @@ while True:
             elif ohio_choices == "3":
                 rolling_cash()
         elif state_choice == "2":
-            pennsylvania_choice = input(
-                "\n1. Cash 4 Life \n2. Match 6 \n3. Treasure Hunt \nWhich Pennsylvania game would you like numbers for? ")
-            if pennsylvania_choice == "1":
-                cash4_life()
-            elif pennsylvania_choice == "2":
-                match6()
-            elif pennsylvania_choice == "3":
-                treasure_hunt()
-        elif state_choice == "3":
             newyork_choice = input(
                 "\n1. Cash 4 Life \n2. New York Lottery \n3. Pick 10 \nWhich New York game would you like numbers for? ")
             if newyork_choice == "1":
@@ -136,17 +168,21 @@ while True:
                 newyork_game()
             elif newyork_choice == "3":
                 pick10()
-        """else:
-            print("Please select your game")"""
+        elif state_choice == "3":
+            pennsylvania_choice = input(
+                "\n1. Cash 4 Life \n2. Match 6 \n3. Treasure Hunt \nWhich Pennsylvania game would you like numbers for? ")
+            if pennsylvania_choice == "1":
+                cash4_life()
+            elif pennsylvania_choice == "2":
+                match6()
+            elif pennsylvania_choice == "3":
+                treasure_hunt()
 
-    again = input("Would you like numbers for another game? ")
-    if again == "yes" or "y":
-        print("\nLet's get more lucky numbers!")
-        continue
-    else:
-        print("Goodbye!")
-        exit()
+    
 
+
+            
+                
 
     
     
